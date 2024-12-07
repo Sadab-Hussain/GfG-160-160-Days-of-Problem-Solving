104bf5ec5b237e349ea2edc17cb132582b5d3423
Sadab-Hussain <sadabhussain0143@gmail.com>
2024-12-07 11:20:40 +0530

    Day 11

---
+++ Day-11/Maximum Product Subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Function to find maximum product subarray
+    int maxProduct(int[] arr) {
+        
+        // code here
+        int n = arr.length;
+        
+        int maxProd = Integer.MIN_VALUE;
+        
+        int leftToRight = 1;
+        
+        int rightToLeft = 1;
+        
+        for(int i=0; i<n; i++){
+            if(leftToRight == 0){
+                leftToRight = 1;
+            }
+            if(rightToLeft == 0){
+                rightToLeft = 1;
+            }
+            
+            leftToRight *= arr[i];
+            
+            int j = n-i-1;
+            rightToLeft *= arr[j];
+            maxProd = Math.max(leftToRight, Math.max(rightToLeft, maxProd));
+        }
+        return maxProd;
+    }
+}
