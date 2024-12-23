0b8c92dcdcab84a38e9fd69a4e34b51834b11168
Sadab-Hussain <sadabhussain0143@gmail.com>
2024-12-23 18:29:06 +0530

    Day 27

---
+++ Day-27/Merge Without Extra Space.java
@@ -0,0 +1,44 @@
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        // code here
+        int n = a.length;
+        int m = b.length;
+        
+        int gap = (n + m + 1) / 2;
+        
+        while(gap > 0){
+            int i = 0, j = gap;
+
+            while (j < n + m) {
+              
+                if (j < n && a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                } 
+              
+                else if (i < n && j >= n && a[i] > b[j - n]) {
+                    int temp = a[i];
+                    a[i] = b[j - n];
+                    b[j - n] = temp;
+                } 
+
+                else if (i >= n && b[i - n] > b[j - n]) {
+                    int temp = b[i - n];
+                    b[i - n] = b[j - n];
+                    b[j - n] = temp;
+                }
+                i++;
+                j++;
+            }
+            
+            if(gap == 1){
+                break;
+            } 
+            
+            gap = (gap + 1)/2;
+        }
+        
+    }
+}
