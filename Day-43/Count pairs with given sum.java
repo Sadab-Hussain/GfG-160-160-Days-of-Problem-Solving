32a4f00064a0ecfa77799006664562c3ddcdc5df
Sadab-Hussain <sadabhussain0143@gmail.com>
2025-01-08 15:06:33 +0530

    Day 43

---
+++ Day-43/Count pairs with given sum.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        // Your code here
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int cnt = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            
+            if(freq.containsKey(target - arr[i])){
+                cnt += freq.get(target - arr[i]);  
+            }
+            
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+        }
+        return cnt;
+    }
+}
