6fb37c576c152070c13d164d765268bd18146d93
Sadab-Hussain <sadabhussain0143@gmail.com>
2025-03-05 11:11:21 +0530

    Day 99

---
+++ Day-99/Parenthesis Checker.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(!st.empty() && ((st.peek() == '(' && s.charAt(i) == ')') ||
+                    (st.peek() == '{' && s.charAt(i) == '}') ||
+                    (st.peek() == '[' && s.charAt(i) == ']'))){
+                    st.pop();        
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+}
